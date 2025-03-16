3935a943fd6a898d2775546c3c7a7971487cd2e4
kenji <hotdrop.tkenji@gmail.com>
2025-03-16 09:47:56 +0900

    androidApp側のリソースを追加

--- shared/src/commonMain/kotlin/jp/hotdrop/considercline/repository/remote/KtorHttpClient.kt
+++ shared/src/commonMain/kotlin/jp/hotdrop/considercline/repository/remote/KtorHttpClient.kt
@@ -3,7 +3,6 @@ package jp.hotdrop.considercline.repository.remote
 import io.ktor.client.HttpClient as KtorClient
 import io.ktor.client.call.body
 import io.ktor.client.plugins.timeout
-import io.ktor.client.request.HttpRequestBuilder
 import io.ktor.client.request.get
 import io.ktor.client.request.parameter
 import io.ktor.client.request.post
